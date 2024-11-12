fee4fa0b1d7dd170dd41eb0cc4da9178bebfad94
GabeHasAGit <GabeDoesWork954@gmail.com>
2024-11-12 17:14:48 -0500

    Finished

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,54 @@
 import pygame
-
+import math
+import random
 
 def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
         # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
-        mole_image = pygame.image.load("mole.png")
+        mole_image = pygame.image.load("whackamole-template/mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+
+        #if it doesnt like it, remove this
+        rColor = random.randint(50, 255)
+        gColor = random.randint(50, 255)
+        bColor = random.randint(50, 255)
+
+        moleX = 0
+        moleY = 0
+
+        x, y = 0, 0
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+            
+            screen.fill([rColor, gColor, bColor])
+
+            color = "black"
+            defaultColor = "light green"
+
+            for x1 in range(32, 640, 32):
+                pygame.draw.line(screen, color, (x1,0), (x1,512))
+            for y1 in range(32, 512, 32):
+                pygame.draw.line(screen, color, (0,y1), (640,y1))
+            
+
+            if x > moleX and x < moleX + 32 and y > moleY and y < moleY + 32:
+                moleX = random.randint(1,19)*32
+                moleY = random.randint(1,15)*32
+                rColor = random.randint(50, 255)
+                gColor = random.randint(50, 255)
+                bColor = random.randint(50, 255)
+
+            screen.blit(mole_image, mole_image.get_rect(topleft=(3+moleX,2+moleY)))
+
             pygame.display.flip()
             clock.tick(60)
     finally:
